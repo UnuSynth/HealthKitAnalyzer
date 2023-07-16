6b78111cf080610a416b1d7a27c90af8ca814906
aabdyshev <aabdyshev@nurtelecom.kg>
2023-07-16 15:13:48 +0600

    init commit

---
+++ HealthKitAnalyzer/HealthKitAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitAnalyzerApp.swift
+//  HealthKitAnalyzer
+//
+//  Created by Abdyshev Amantay on 16/7/23.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
